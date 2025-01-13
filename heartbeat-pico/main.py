576b38a5095f418c826b108c178d3a5d16c42d25
Mițca Dumitru <dumitru0mitca@gmail.com>
2025-01-14 01:34:12 +0200

    pico: Try to get the pulse sensor working

--- heartbeat-pico/main.py
+++ heartbeat-pico/main.py
@@ -4,12 +4,12 @@ import sys
 sys.path.append("")
 
 from micropython import const
+from machine import ADC, Pin
 
 import asyncio
 import aioble
 import bluetooth
-
-import random
+import time
 import struct
 
 _HEART_RATE_UUID = bluetooth.UUID(0x180D)
@@ -22,24 +22,52 @@ _ADV_INTERVAL_MS = 250_000
 
 # Register GATT server.
 heart_rate_service = aioble.Service(_HEART_RATE_UUID)
-temp_characteristic = aioble.Characteristic(
+bpm_characteristic = aioble.Characteristic(
     heart_rate_service, _HEART_RATE_SENSOR_UUID, read=True, notify=True
 )
 aioble.register_services(heart_rate_service)
 
 
 # Helper to encode the temperature characteristic encoding (sint16, hundredths of a degree).
-def _encode_temperature(temp_deg_c):
-    return struct.pack("<h", int(temp_deg_c * 100))
+def _encode_bpm(temp_deg_c):
+    return struct.pack("<B", int(temp_deg_c))
 
 
-# This would be periodically polling a hardware sensor.
+# based on this blog post https://peppe8o.com/pulse-sensor-with-raspberry-pi-pico-hearth-beat-chech-with-micropython/
 async def sensor_task():
-    t = 24.5
+    adc = ADC(28)
+
+    max_samples = 2500
+    short_average = 15
+    long_average = 100
+    beat_threshold = 200
+    finger_threshold = 30000
+    history = []
+
+    last_time = time.ticks_ms()
+
+    beats = 0
+
     while True:
-        temp_characteristic.write(_encode_temperature(t), send_update=True)
-        t += random.uniform(-0.5, 0.5)
-        await asyncio.sleep_ms(1000)
+        v = adc.read_u16()
+        history.append(v)
+        history = history[-max_samples:]
+
+        current_time = time.ticks_ms()
+
+        if max(history) - min(history) < finger_threshold:
+            avg_1 = sum(history[-short_average:]) / short_average
+            avg_2 = sum(history[-long_average:]) / long_average
+
+            if avg_1 - avg_2 > beat_threshold:
+                beats += 1
+
+        if time.ticks_diff(current_time, last_time) >= 60_000:
+            last_time = current_time
+            print(f"{beats} bpm")
+            bpm_characteristic.write(_encode_bpm(beats), send_update=True)
+            beats = 0
+            await asyncio.sleep_ms(10)  # yield to other tasks for a bit
 
 
 # Serially wait for connections. Don't advertise while a central is
@@ -58,6 +86,11 @@ async def peripheral_task():
 
 # Run both tasks.
 async def main():
+    print(f"{_HEART_RATE_UUID}, {_HEART_RATE_SENSOR_UUID}")
+
+    for characteristic in heart_rate_service.characteristics:
+        print(characteristic.uuid)
+
     t1 = asyncio.create_task(sensor_task())
     t2 = asyncio.create_task(peripheral_task())
     await asyncio.gather(t1, t2)
